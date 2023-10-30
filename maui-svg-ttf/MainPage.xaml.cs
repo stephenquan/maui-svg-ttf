c61ecc7035c6f76405351f6ac31ef3c24b30dcc4
Stephen Quan <squan@esri.com>
2023-10-30 17:27:33 +1100

    Color changer

--- maui-svg-ttf/MainPage.xaml.cs
+++ maui-svg-ttf/MainPage.xaml.cs
@@ -2,8 +2,24 @@
 
 public partial class MainPage : ContentPage
 {
+    public Color Color => Color.FromRgb(RedValue, 0, 0);
+
+    private int _redValue = 128;
+    public int RedValue
+    {
+        get => _redValue;
+        set
+        {
+            _redValue = value;
+            OnPropertyChanged(nameof(RedValue));
+            OnPropertyChanged(nameof(Color));
+        }
+    }
+
+
     public MainPage()
     {
         InitializeComponent();
+        BindingContext = this;
     }
 }
